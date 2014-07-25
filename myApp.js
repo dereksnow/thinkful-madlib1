f7cac959f262363ee49d272251ca387548e0807e
derek <derek.snow@gmail.com>
2014-07-25 09:03:00 -0500

    Added controller for modularity/separation of concerns

---
+++ myApp.js
@@ -0,0 +1,21 @@
+angular.module('myApp', [])
+    .controller('InputCtrl', function($scope) {
+        $scope.input = {
+            title_rank: "title or rank",
+            pet_name: "pet's name",
+            name: "name",
+            place: "place",
+            verb1: "verb",
+            food: "food plural",
+            verb2: "verb",
+            verb3: "verb",
+            noun1: "noun",
+            brand1: "brand",
+            animal: "animal singular possessive",
+            clothing: "article of clothing",
+            brand2: "brand",
+            animal2: "animal singular possessive",
+            body_part: "body part",
+            verb4: "verb present participle"
+        }
+    });
