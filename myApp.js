da823700136d80810b3eb7fbdf32af0e0d2afee6
derek <derek.snow@gmail.com>
2014-07-29 08:56:51 -0500

    Better ui implementation with submit and reset functions

--- myApp.js
+++ myApp.js
@@ -1,21 +1,23 @@
 angular.module('myApp', [])
-    .controller('InputCtrl', function($scope) {
-        $scope.input = {
-            title_rank: "title or rank",
-            pet_name: "pet's name",
-            name: "name",
-            place: "place",
-            verb1: "verb",
-            food: "food plural",
-            verb2: "verb",
-            verb3: "verb",
-            noun1: "noun",
-            brand1: "brand",
-            animal: "animal singular possessive",
-            clothing: "article of clothing",
-            brand2: "brand",
-            animal2: "animal singular possessive",
-            body_part: "body part",
-            verb4: "verb present participle"
+    .controller('FormCtrl', ['$scope',
+        function($scope) {
+            $scope.submit = function() {
+                if ($scope.myForm.$valid) {
+                    console.log('The form is valid');
+		    $scope.results = true;
+		    $scope.inputFrm = true;
+		    $scope.submitted = false;
+                } else {
+                    console.log('The form is invalid');
+                }
+            };
+	    $scope.reset = function() {
+		    $scope.results = false;
+		    $scope.inputFrm = false;
+		    $scope.data = {};
+		    $scope.myForm.$setPristine();
+		    $scope.submitted = false;
+	    };
+
         }
-    });
+    ]);
